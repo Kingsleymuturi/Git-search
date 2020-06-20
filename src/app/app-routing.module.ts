6b5d97a8098ed2a992ed0f4c06d88a63164d511a
Kingsleymuturi <kingsleymuturi9@gmail.com>
2020-06-20 18:00:05 +0300

    add about route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { UsersComponent } from './users/users.component';
 import {  NotfoundComponent } from './notfound/notfound.component';
 import {ReposComponent} from './repos/repos.component'
-
+import {AboutComponent} from './about/about.component'
 
 const routes: Routes = [
   {path:"user-profile", component:UsersComponent, },
   {path:"repos-search", component:ReposComponent},
+  {path:"about", component:AboutComponent},
   {path: "", redirectTo:"/user-profile", pathMatch:"full"},
   {path:"**",component:NotfoundComponent}
 ];
